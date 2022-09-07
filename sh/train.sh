dc3d4a84c73bbfaafbcb75d9d85e3d64948b637a
御姐 <162095214@qq.com>
2022-09-07 15:08:26 +0800

    add train

---
+++ sh/train.sh
@@ -0,0 +1,18 @@
+python ../trainer.py \
+  --input \
+  data/train/train.zh.bpe.min \
+  data/train/train.en.bpe.min \
+  --vocabulary \
+  data/nist.20k.zh.vocab \
+  data/nist.10k.en.vocab \
+  --model \
+  transformer \
+  --validation \
+  data/val/val.zh.bpe \
+  --references \
+  data/val/val.en.* \
+  --process_group \
+  gloo \
+  --parameters=batch_size=16,update_cycle=2 \
+  --hparam_set \
+  base
